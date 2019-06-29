d9372b089f86edeb9a9e93f5dd80c083043dec09
ely <ely137137@gmail.com>
2019-06-29 17:41:41 +0300

    Фильтр

--- js/preview.js
+++ js/preview.js
@@ -106,7 +106,7 @@
         filter = 'blur(' + (effectLevelInput.value / 100) * 3 + 'px)';
         break;
       case 'heat':
-        filter = (effectLevelInput.value > 34) ? 'brightness(' + (effectLevelInput.value / 100) * 3 + ')' : 'brightness(' + 1 + ')';
+        filter = 'brightness(' + (((effectLevelInput.value / 100) * 2) + 1) + ')';
         break;
     }
 
