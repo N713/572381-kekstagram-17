22dffbd01f4064aab8881d23ef097e285e09480c
ely <ely137137@gmail.com>
2019-07-04 14:57:57 +0300

    Правки

--- js/backend.js
+++ js/backend.js
@@ -2,17 +2,7 @@
 
 (function () {
 
-  var filters = document.querySelector('.img-filters');
-
-  window.photos = [];
-
-  var renderPhotos = function (data) {
-    window.photos = data;
-    filters.classList.remove('img-filters--inactive');
-    window.render(window.photos);
-  };
-
-  var onErrorHandler = function (errorMessage) {
+  window.onErrorHandler = function (errorMessage) {
     var node = document.createElement('div');
     node.style = 'z-index: 100; margin: 0 auto; text-align: center; background-color: red;';
     node.style.position = 'absolute';
@@ -25,7 +15,5 @@
     node.textContent = errorMessage;
     document.body.insertAdjacentElement('afterbegin', node);
   };
-
-  window.load(renderPhotos, onErrorHandler);
 
 })();
--- js/gallery.js
+++ js/gallery.js
@@ -1,9 +1,40 @@
 'use strict';
 
 (function () {
-  var picturesSection = document.querySelector('.pictures');
 
-  window.addPictures = function (photos) {
+  var URL = 'https://js.dump.academy/kekstagram/data';
+  var picturesSection = document.querySelector('.pictures');
+  var filters = document.querySelector('.img-filters');
+
+  window.photos = [];
+
+  var load = function (onSuccess, onError) {
+    var xhr = new XMLHttpRequest();
+
+    xhr.responseType = 'json';
+
+    xhr.addEventListener('load', function () {
+      if (xhr.status === 200) {
+        onSuccess(xhr.response);
+      } else {
+        onError('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
+      }
+    });
+
+    xhr.addEventListener('error', function () {
+      onError('Произошла ошибка соединения');
+    });
+    xhr.addEventListener('timeout', function () {
+      onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
+    });
+
+    xhr.timeout = 10000;
+
+    xhr.open('GET', URL);
+    xhr.send();
+  };
+
+  var addPictures = function (photos) {
     var fragment = document.createDocumentFragment();
 
     for (var i = 0; i < photos.length; i++) {
@@ -14,9 +45,16 @@
   };
 
   window.render = function (array) {
-    picturesSection.appendChild(window.addPictures(array));
+    picturesSection.appendChild(addPictures(array));
+  };
+
+  var renderPhotos = function (data) {
+    window.photos = data;
+    filters.classList.remove('img-filters--inactive');
+    window.render(window.photos);
   };
 
   window.picturesSection = picturesSection;
+  load(renderPhotos, window.onErrorHandler);
 
 })();
